91acce3866f53de28db02d5ff93cc5be267bed56
NotProvided98 <emailpolicedot@gmail.com>
2024-12-05 09:37:25 +0200

    Update script.js

--- folder/script.js
+++ folder/script.js
@@ -6,10 +6,8 @@ document.getElementById("file-input").addEventListener("change", function (event
         reader.onload = function (e) {
             const image = new Image();
             image.onload = function () {
-                // Display the uploaded image in the left container
                 document.getElementById("uploaded-image").src = e.target.result;
-
-                // Extract EXIF data from the image using EXIF.js
+                
                 EXIF.getData(image, function () {
                     // Populate fields with the EXIF data if available
                     document.getElementById("application_record_version").value = EXIF.getTag(this, 'Software') || "No software";
@@ -25,7 +23,6 @@ document.getElementById("file-input").addEventListener("change", function (event
     }
 });
 
-// Add the logic to download the updated image with modified EXIF data
 document.getElementById("download-button").addEventListener("click", function () {
     const image = document.getElementById("uploaded-image");
 
@@ -37,11 +34,9 @@ document.getElementById("download-button").addEventListener("click", function ()
     const newLightSource = parseInt(document.getElementById("light_source").value);
     console.log(newSoftware, newDate, newLightSource, newMake, newModel);
 
-    // Create a new EXIF object
     let exifObj = piexif.load(image.src);
     console.log(exifObj);
 
-    // Modify EXIF data
     exifObj['0th'][piexif.ImageIFD.Software] = newSoftware;
     exifObj['0th'][piexif.ImageIFD.DateTime] = newDate;
     exifObj['0th'][piexif.ImageIFD.Make] = newMake;
@@ -54,16 +49,8 @@ document.getElementById("download-button").addEventListener("click", function ()
     // Embed the modified EXIF data into the image
     const newImageData = piexif.insert(exifBytes, image.src);
 
-    // Create a downloadable link for the new image with updated EXIF data
     const link = document.createElement("a");
     link.href = newImageData;
-    link.download = "modified_image.jpg"; // or the file type of the image
+    link.download = "modified_image.jpg";
     link.click();
-});
-
-// Optionally, add event listeners for input fields to dynamically update the values
-document.getElementById("light_source").addEventListener("input", function () {
-    const newLightSource = parseInt(this.value) || 0;
-    console.log("LightSource changed to:", newLightSource);
-    // You could store this value in a global variable or just update dynamically
 });
